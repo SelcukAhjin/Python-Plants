7c72946b3721ce8c0fad7ebad990908de4513f62
Ilker Selcuk Akyüz <selcukilkerakyuez@gmail.com>
2026-06-25 10:56:47 +0200

    feat: Return common name from plant search results

--- api_service.py
+++ api_service.py
@@ -1,3 +1,5 @@
+from os import name
+
 import requests as rq
 
 API_KEY = "sk-8VDw6a3ccec036ac818405"
@@ -5,6 +7,10 @@ API_KEY = "sk-8VDw6a3ccec036ac818405"
 def suchePflanze(suchbegriff):
     antwort = rq.get(f"https://perenual.com/api/species-list?key={API_KEY}&q={suchbegriff}")
     rohDaten = antwort.json()
-    
+    schritt1 = rohDaten["data"]
+    schritt2 = schritt1[0]
+    name = schritt2["common_name"]
+    return name
+
 
 print(suchePflanze("monstera"))
