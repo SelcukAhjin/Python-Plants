11043b3991050f4204ebc8509a70a62072868fcf
Ilker Selcuk Akyüz <selcukilkerakyuez@gmail.com>
2026-06-25 12:32:38 +0200

    feat: add rate limit error handling for Perenual API

--- api_service.py
+++ api_service.py
@@ -7,9 +7,21 @@ def suchePflanze(suchbegriff):
     name = Daten["common_name"]
     sciName = Daten['scientific_name'][0]
     id = Daten['id']
+    getPflegeDaten(id)
     return name,sciName
 
 
+def getPflegeDaten(pflanzen_id):
+    antwort = rq.get(f"https://perenual.com/api/species/details/{pflanzen_id}?key={API_KEY}")
+    if antwort.status_code == 200:
+        rohDaten = antwort.json()
+        schritt1 = rohDaten["data"]
+        schritt2 = schritt1[0]
+        return schritt2
+    else:
+        print(antwort.status_code)
+        print(antwort.text)
+        return "Nicht verfügbar (API LIMIT)"
 
 def getRohDaten(suchbegriff):
     antwort = rq.get(f"https://perenual.com/api/species-list?key={API_KEY}&q={suchbegriff}")
@@ -17,3 +29,5 @@ def getRohDaten(suchbegriff):
     schritt1 = rohDaten["data"]
     schritt2 = schritt1[0]
     return schritt2
+
+a,b=suchePflanze("monstera")
